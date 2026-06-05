1087587e9cf8fe8c60dabf99352db7cef7db70e6
Bidyadhar Jena <bidyadharj2@gmail.com>
2026-06-05 17:36:25 +0530

    Added Task 5 SQL joins queries.
    Implemented SQL join operations including INNER JOIN, LEFT JOIN, RIGHT JOIN, FULL OUTER JOIN simulation using UNION, and multi-table joins using the Library Management System database.

---
+++ Library_Management-5.sql
@@ -0,0 +1,66 @@
+-- Library Management System --
+-- _________________________ --
+
+-- Project of Task 3 for Elevate Labs SQL Internship --
+-- Submitting by Bidyadhar Jena --
+-- ________________________________________ --
+
+USE Library;
+
+-- INNER JOIN
+
+SELECT Students.student_name,
+Books.title,
+Borrow.borrow_date
+FROM Borrow
+INNER JOIN Students
+ON Borrow.student_id = Students.student_id
+INNER JOIN Books
+ON Borrow.book_id = Books.book_id;
+
+-- LEFT JOIN
+
+SELECT Students.student_name,
+Borrow.borrow_date
+FROM Students
+LEFT JOIN Borrow
+ON Students.student_id = Borrow.student_id;
+
+-- RIGHT JOIN
+
+SELECT Books.title,
+Borrow.borrow_date
+FROM Borrow
+RIGHT JOIN Books
+ON Borrow.book_id = Books.book_id;
+
+-- FULL OUTER JOIN
+--(MySQL does not support FULL OUTER JOIN directly
+-- Using UNION to simulate FULL OUTER JOIN )
+
+SELECT Students.student_name,
+Borrow.borrow_date
+FROM Students
+LEFT JOIN Borrow
+ON Students.student_id = Borrow.student_id
+
+UNION
+
+SELECT Students.student_name,
+Borrow.borrow_date
+FROM Students
+RIGHT JOIN Borrow
+ON Students.student_id = Borrow.student_id;
+
+-- JOIN with multiple tables
+
+SELECT Students.student_name,
+Books.title,
+Authors.author_name
+FROM Borrow
+INNER JOIN Students
+ON Borrow.student_id = Students.student_id
+INNER JOIN Books
+ON Borrow.book_id = Books.book_id
+INNER JOIN Authors
+ON Books.author_id = Authors.author_id;
